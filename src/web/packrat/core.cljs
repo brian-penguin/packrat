320c7584935c38a6ff1aaa4c87b31b1811425b5f
Brian Tenggren <btenggren20@gmail.com>
2020-11-26 17:50:43 -0500

    Just adding a tiny header

--- src/web/packrat/core.cljs
+++ src/web/packrat/core.cljs
@@ -6,14 +6,23 @@
   [:h1
    [:span.text-3xl.text-pink-500 message]])
 
+(defn header []
+  [:div.bg-pink-400
+   [:span.text-white.text-3xl "Packrat"]])
+
 (defn simple-example-component []
   [:div
    [bold-greeting "Hello World"]
    [:p "Nice to see you"]])
+
+(defn html []
+  [:div.main
+   [header]
+   [simple-example-component]])
 
 (def app-dom-root
   (js/document.getElementById "app"))
 
 (defn ^:export ^:dev/after-load run []
   (js/console.log "Run Called!")
-  (rdom/render [simple-example-component] app-dom-root))
+  (rdom/render [html] app-dom-root))
